c33a5a8f40f311a184c45908fe73c25c82f96672
Anton1256 <anton.bidenko1@gmail.com>
2025-04-18 19:37:59 +0300

    hw done

--- js/task-1.js
+++
@@ -1,9 +0,0 @@
-const categoriesList = document.querySelector("#categories");
-const categoriesItems = categoriesList.querySelectorAll(".item");
-
-console.log(`Категорій: ${categoriesItems.length}`);
-
-categoriesItems.forEach((item) => {
-    const title = item.querySelector('h2').textContent;
-    console.log(`Категорія: ${title}`)
-});
